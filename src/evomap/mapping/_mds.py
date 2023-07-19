ebaf98923c82810313f53887a376582c5a67c0e8
Maximilian Matthe <mpmatthe@iu.edu>
2023-07-19 13:52:32 -0400

    Update folder

--- src/evomap/mapping/_mds.py
+++ src/evomap/mapping/_mds.py
@@ -175,7 +175,8 @@ def _normalized_stress_function(
         distances_flat = distances[np.tril_indices(len(distances),-1)]
         disparities_flat = disparities[np.tril_indices(len(disparities),-1)]        
 
-        disp_hat = ir.fit_transform(distances_flat, disparities_flat)
+        disp_hat = ir.fit_transform(X = disparities_flat, y = distances_flat)
+        #TOOD: Rename disparities to dissimilarities, before fitting
         disp_hat = disp_hat.reshape(-1)
         disp_hat = normalize_dhat(disp_hat, n_samples)
 
